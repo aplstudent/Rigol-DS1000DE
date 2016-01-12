14717add2f21b7113af476ecce505d84c894f166
Brianjp93 <brianjp93@gmail.com>
2016-01-12 15:27:46 -0800

    Initial Tests

---
+++ rigolSkeleton.py
@@ -0,0 +1,16 @@
+"""
+rigolSkeleton.py
+Brian Perrett
+Advanced Projects Lab, University of Oregon
+January 12, 2016
+
+to be used on top of other classes as the means of writing commands
+	to and retrieving from the Rigol DS1102E
+"""
+import usb.core
+import usb.util
+
+class RigolSkeleton:
+
+	def __init__(self):
+		pass
