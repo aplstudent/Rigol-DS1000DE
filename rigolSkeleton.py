50fabd7eb89ec271bbc258afebb546eefbc1b115
Brianjp93 <brianjp93@gmail.com>
2016-01-12 16:20:17 -0800

    slight changes

--- rigolSkeleton.py
+++ rigolSkeleton.py
@@ -5,12 +5,13 @@ Advanced Projects Lab, University of Oregon
 January 12, 2016
 
 to be used on top of other classes as the means of writing commands
-	to and retrieving from the Rigol DS1102E
+    to and retrieving from the Rigol DS1102E
 """
 import usb.core
 import usb.util
 
+
 class RigolSkeleton:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
